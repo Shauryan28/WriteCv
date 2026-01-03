12c4eccf5ba063bd21978ca477a6e618596fa08b
Shaur <shaur@writecv.local>
2026-01-04 00:34:40 +0530

    fix: Use process.cwd() for robust static file path resolution

--- server.js
+++ server.js
@@ -273,12 +273,14 @@ app.post('/api/generate', async (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'dist')));
+const distPath = path.join(process.cwd(), 'dist');
+console.log('Serving uploaded files from:', distPath);
+app.use(express.static(distPath));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(distPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
