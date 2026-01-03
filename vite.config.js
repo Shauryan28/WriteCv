e195b53e34ea2f112fd8db9a9021ed1afa017ec6
Shaur <shaur@writecv.local>
2026-01-04 00:24:27 +0530

    chore: Move index.html to src to prevent hosting conflicts

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3001'
